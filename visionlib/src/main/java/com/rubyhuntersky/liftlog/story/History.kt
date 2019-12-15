41dd6491be8d96d121913501303a5e06139b7c89
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-12-14 22:30:22 -0800

    Spot days and rounds from history.

--- visionlib/src/main/java/com/rubyhuntersky/liftlog/story/History.kt
+++ visionlib/src/main/java/com/rubyhuntersky/liftlog/story/History.kt
@@ -1,7 +1,56 @@
 package com.rubyhuntersky.liftlog.story
 
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
 data class History(
     val movements: Set<Movement>
 ) {
     fun add(movement: Movement) = copy(movements = movements + movement)
+
+    val logDays: List<LogDay> by lazy {
+        val groupedByDay = movements.groupBy { toMilliDate(it.milliTime) }
+        val sorted = groupedByDay.entries.sortedBy { it.key }
+        val rounds = sorted.map { (_, movements) -> toRounds(movements) }
+        rounds.map {
+            LogDay(it)
+        }
+    }
 }
+
+@ExperimentalTime
+private fun toRounds(dayMovements: List<Movement>): List<Round> {
+    val (milliTime, preround, rounds) = dayMovements.sortedBy(Movement::milliTime)
+        .fold(
+            initial = Triple(0L, emptyList<Movement>(), emptyList<Round>()),
+            operation = { (milliTime, preround, rounds), movement ->
+                val elapsed = movement.milliTime - milliTime
+                if (elapsed > milliTimePerHalfHour) {
+                    if (preround.isEmpty()) {
+                        Triple(movement.milliTime, listOf(movement), rounds)
+                    } else {
+                        val nextRounds = rounds + Round(milliTime, preround)
+                        Triple(movement.milliTime, listOf(movement), nextRounds)
+                    }
+                } else {
+                    val newPreround = preround + movement
+                    Triple(movement.milliTime, newPreround, rounds)
+                }
+            }
+        )
+    val lastRound = if (preround.isNotEmpty()) Round(milliTime, preround) else null
+    return lastRound?.let { rounds + it } ?: rounds
+}
+
+@ExperimentalTime
+fun toMilliDate(milliTime: Long): Long = milliTime / milliTimePerDay
+
+@ExperimentalTime
+private val milliTimePerDay =
+    Duration.convert(1.0, DurationUnit.DAYS, DurationUnit.MILLISECONDS).toLong()
+
+@ExperimentalTime
+private val milliTimePerHalfHour =
+    Duration.convert(30.0, DurationUnit.MINUTES, DurationUnit.MILLISECONDS).toLong()
--- visionlib/src/test/kotlin/com/rubyhuntersky/liftlog/story/HistoryTest.kt
+++ visionlib/src/test/kotlin/com/rubyhuntersky/liftlog/story/HistoryTest.kt
@@ -3,15 +3,41 @@ package com.rubyhuntersky.liftlog.story
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.util.*
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
 internal class HistoryTest {
 
     private val now = Date().time
+    private val yesterday =
+        now - Duration.convert(1.0, DurationUnit.DAYS, DurationUnit.MILLISECONDS).toLong()
+    private val hourAgo =
+        now - Duration.convert(1.0, DurationUnit.HOURS, DurationUnit.MILLISECONDS).toLong()
 
     @Test
     fun addMovement() {
         val movement = Movement(now, Direction.PullUps, Force.Lbs(100), Distance.Reps(5))
         val history = History(emptySet()).add(movement)
         assertEquals(1, history.movements.size)
+    }
+
+    @Test
+    fun daySpotting() {
+        val movement1 = Movement(now, Direction.PullUps, Force.Lbs(100), Distance.Reps(3))
+        val movement2 = Movement(yesterday, Direction.PullUps, Force.Lbs(100), Distance.Reps(1))
+        val history = History(setOf(movement1, movement2))
+        val days = history.logDays
+        assertEquals(2, days.size)
+    }
+
+    @Test
+    fun largeGapRoundDetection() {
+        val movement1 = Movement(now, Direction.PullUps, Force.Lbs(100), Distance.Reps(3))
+        val movement2 = Movement(hourAgo, Direction.PullUps, Force.Lbs(100), Distance.Reps(1))
+        val history = History(setOf(movement1, movement2))
+        val rounds = history.logDays.first().rounds
+        assertEquals(2, rounds.size)
     }
 }
